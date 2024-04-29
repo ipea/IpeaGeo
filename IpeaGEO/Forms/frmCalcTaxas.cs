90ef576e53205ccc62cd57bfa2000ef2c694ebbf
AlexandreYwata <alexandre.ywata@ipea.gov.br>
2024-04-29 14:21:34 -0300

    Update frmCalcTaxas.cs

--- IpeaGEO/Forms/frmCalcTaxas.cs
+++ IpeaGEO/Forms/frmCalcTaxas.cs
@@ -18,37 +18,30 @@ namespace IpeaGEO.Forms
 
         private void frmCalcTaxas_Load(object sender, EventArgs e)
         {
-
         }
 
         private void btnAbrirTabelaDados_Click(object sender, EventArgs e)
         {
-
         }
 
         private void btnExecutar_Click(object sender, EventArgs e)
         {
-
         }
 
         private void splitContainer2_SplitterMoved(object sender, SplitterEventArgs e)
         {
-
         }
 
         private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
-
         }
 
         private void label6_Click(object sender, EventArgs e)
         {
-
         }
     }
 }
