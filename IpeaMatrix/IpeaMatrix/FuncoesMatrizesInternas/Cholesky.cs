b063e50c69a255cb0468f748cb5391cbc55b9b50
AlexandreYwata <alexandre.ywata@ipea.gov.br>
2024-04-29 14:38:55 -0300

    Update Cholesky.cs

--- IpeaMatrix/IpeaMatrix/FuncoesMatrizesInternas/Cholesky.cs
+++ IpeaMatrix/IpeaMatrix/FuncoesMatrizesInternas/Cholesky.cs
@@ -20,21 +20,21 @@ namespace IpeaMatrix
             n = a.GetLength(0);
             el = a.Clone();
 
-		    int i,j,k;
-		    double sum;
-		    if (el.ncols() != n) throw new Exception("need square matrix");
-		    for (i=0;i<n;i++) {
-			    for (j=i;j<n;j++) {
-				    for (sum=el[i,j],k=i-1;k>=0;k--) sum -= el[i,k]*el[j,k];
-				    if (i == j) {
-					    if (sum <= 0.0)
-						    throw new Exception("Cholesky failed");
-					    el[i,i]=Math.Sqrt(sum);
-				    } else el[j,i]=sum/el[i,i];
-			    }
+	    int i,j,k;
+	    double sum;
+	    if (el.ncols() != n) throw new Exception("need square matrix");
+	    for (i=0;i<n;i++) {
+		    for (j=i;j<n;j++) {
+			    for (sum=el[i,j],k=i-1;k>=0;k--) sum -= el[i,k]*el[j,k];
+			    if (i == j) {
+				    if (sum <= 0.0)
+					    throw new Exception("Cholesky failed");
+				    el[i,i]=Math.Sqrt(sum);
+			    } else el[j,i]=sum/el[i,i];
 		    }
-		    for (i=0;i<n;i++) for (j=0;j<i;j++) el[j,i] = 0.0;
 	    }
+	    for (i=0;i<n;i++) for (j=0;j<i;j++) el[j,i] = 0.0;
+        }
 
         /// <summary>
         /// Solve the set of n linear equations Ax = b, where a is a positive-definite symmetric matrix
@@ -43,64 +43,64 @@ namespace IpeaMatrix
         /// </summary>
         /// <param name="b">Right-hand-side coefficients.</param>
         /// <param name="x">Solution coefficients.</param>
-	    public void solve(ref Matrix b, ref Matrix x) 
+        public void solve(ref Matrix b, ref Matrix x) 
         {
-		    int i,k;
-		    double sum;
-		    if (b.size() != n || x.size() != n) throw  new Exception("bad lengths in Cholesky");
-		    for (i=0;i<n;i++) {
-			    for (sum=b[i],k=i-1;k>=0;k--) sum -= el[i,k]*x[k];
-			    x[i]=sum/el[i,i];
-		    }
-		    for (i=n-1;i>=0;i--) {
-			    for (sum=x[i],k=i+1;k<n;k++) sum -= el[k,i]*x[k];
-			    x[i]=sum/el[i,i];
-		    }		
+	    int i,k;
+	    double sum;
+	    if (b.size() != n || x.size() != n) throw  new Exception("bad lengths in Cholesky");
+	    for (i=0;i<n;i++) {
+		    for (sum=b[i],k=i-1;k>=0;k--) sum -= el[i,k]*x[k];
+		    x[i]=sum/el[i,i];
 	    }
+	    for (i=n-1;i>=0;i--) {
+		    for (sum=x[i],k=i+1;k<n;k++) sum -= el[k,i]*x[k];
+		    x[i]=sum/el[i,i];
+	    }		
+        }
 
-	    public void elmult(ref Matrix y, ref Matrix b) 
+        public void elmult(ref Matrix y, ref Matrix b) 
         {
-		    int i,j;
-		    if (b.size() != n || y.size() != n) throw new Exception("bad lengths");
-		    for (i=0;i<n;i++) {
-			    b[i] = 0.0;
-			    for (j=0;j<=i;j++) b[i] += el[i,j]*y[j];
-		    }
+	    int i,j;
+	    if (b.size() != n || y.size() != n) throw new Exception("bad lengths");
+	    for (i=0;i<n;i++) {
+		    b[i] = 0.0;
+		    for (j=0;j<=i;j++) b[i] += el[i,j]*y[j];
 	    }
+        }
 
-	    public void elsolve(ref Matrix b, ref Matrix y) 
+        public void elsolve(ref Matrix b, ref Matrix y) 
         {
-		    int i,j;
-		    double sum;
-		    if (b.size() != n || y.size() != n) throw new Exception("bad lengths");
-		    for (i=0;i<n;i++) {
-			    for (sum=b[i],j=0; j<i; j++) sum -= el[i,j]*y[j];
-			    y[i] = sum/el[i,i];
-		    }
+	    int i,j;
+	    double sum;
+	    if (b.size() != n || y.size() != n) throw new Exception("bad lengths");
+	    for (i=0;i<n;i++) {
+		    for (sum=b[i],j=0; j<i; j++) sum -= el[i,j]*y[j];
+		    y[i] = sum/el[i,i];
 	    }
+        }
 
-	    public void inverse(ref Matrix ainv) 
+	public void inverse(ref Matrix ainv) 
         {
-		    int i,j,k;
-		    double sum;
-		    ainv.resize(n,n);
-		    for (i=0;i<n;i++) for (j=0;j<=i;j++){
-			    sum = (i==j? 1.0 : 0.0);
-			    for (k=i-1;k>=j;k--) sum -= el[i,k]*ainv[j,k];
-			    ainv[j,i]= sum/el[i,i];
-		    }
-		    for (i=n-1;i>=0;i--) for (j=0;j<=i;j++){
-			    sum = (i<j? 0.0 : ainv[j,i]);
-			    for (k=i+1;k<n;k++) sum -= el[k,i]*ainv[j,k];
-			    ainv[i,j] = ainv[j,i] = sum/el[i,i];
-		    }				
+	    int i,j,k;
+	    double sum;
+	    ainv.resize(n,n);
+	    for (i=0;i<n;i++) for (j=0;j<=i;j++){
+		    sum = (i==j? 1.0 : 0.0);
+		    for (k=i-1;k>=j;k--) sum -= el[i,k]*ainv[j,k];
+		    ainv[j,i]= sum/el[i,i];
 	    }
+	    for (i=n-1;i>=0;i--) for (j=0;j<=i;j++){
+		    sum = (i<j? 0.0 : ainv[j,i]);
+		    for (k=i+1;k<n;k++) sum -= el[k,i]*ainv[j,k];
+		    ainv[i,j] = ainv[j,i] = sum/el[i,i];
+	    }				
+	}
 
-	    public double logdet() 
+	public double logdet() 
         {
-		    double sum = 0.0;
-		    for (int i=0; i<n; i++) sum += Math.Log(el[i,i]);
-		    return 2.0*sum;
-	    }
+	    double sum = 0.0;
+	    for (int i=0; i<n; i++) sum += Math.Log(el[i,i]);
+	    return 2.0*sum;
+	}
     }
 }
